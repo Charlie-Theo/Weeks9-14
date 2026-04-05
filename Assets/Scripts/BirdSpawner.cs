c1eb4afeb6460a6ee6ecec4c1f51ef0d0dc2e83e
CHARLAPTOP\charl <theodouc@sheridancollege.ca>
2026-04-04 22:22:49 -0400

    Added mouse tracking. Can detect which bird the player is clicking via debug log.

--- Assets/Scripts/BirdSpawner.cs
+++ Assets/Scripts/BirdSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BirdSpawner : MonoBehaviour
 {
@@ -14,6 +15,9 @@ public class BirdSpawner : MonoBehaviour
     float timerValue = 0;
 
     public TextMeshProUGUI birdCounter;
+
+    SpriteRenderer birdSR;
+    Vector2 lerpPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,7 +33,7 @@ public class BirdSpawner : MonoBehaviour
         if (timerValue > timerMax )
         {
             spawnPos.x = Random.Range(-9, 9);
-            spawnPos.y = Random.Range(-5, 5);
+            spawnPos.y = Random.Range(-4, 4);
 
             spawnedBird = Instantiate(birdPrefab, spawnPos, Quaternion.identity);
             birds.Add(spawnedBird);
@@ -37,6 +41,20 @@ public class BirdSpawner : MonoBehaviour
             birdCounter.text = "Number of Birds: " + birds.Count;
 
             timerValue = 0;
+        }
+
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        for (int b = 0;  b < birds.Count; b++)
+        {
+            birdSR = birds[b].GetComponent<SpriteRenderer>();
+
+            if (birdSR.bounds.Contains(mousePos) == true && Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                lerpPos = mousePos;
+                Debug.Log(b);
+            }
         }
     }
 }
