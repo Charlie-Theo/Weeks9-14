85430d993a37f7f76df701d604510071bfa98727
CHARLAPTOP\charl <theodouc@sheridancollege.ca>
2026-04-04 23:28:31 -0400

    Made reference from ball to bird spawner script.

--- Assets/Scripts/ThrowingBall.cs
+++ Assets/Scripts/ThrowingBall.cs
@@ -2,17 +2,24 @@ using UnityEngine;
 
 public class ThrowingBall : MonoBehaviour
 {
-
+    BirdSpawner birdScript;
+    Vector2 lerpPos1;
+    Vector2 lerpPos2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        birdScript = GetComponent<BirdSpawner>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lerpPos1 = birdScript.lerpPos;
+
+        if (lerpPos1 != null)
+        {
+            
+        }
     }
 }
